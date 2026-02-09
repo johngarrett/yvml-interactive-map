7ae6781d5cb79e1f20b09f80b3273f5234587af0
garrepi <jacgarrett18@gmail.com>
2026-02-09 12:28:58 -0800

    connect miniplayer

--- src/miniplayer/miniplayer.ts
+++ src/miniplayer/miniplayer.ts
@@ -1,23 +1,38 @@
+import type { POI } from "../types";
+
 export class MiniPlayer {
-    display(entry: unknown) {
+    constructor() {
+        const closeButton = document.getElementById("mini-player-close");
+        if (!closeButton) {
+            throw Error("no close button element found");
+        }
+
+        closeButton.addEventListener("click", () => {
+            this.close();
+        });
+    }
+    display(entry: POI) {
         const element = document.getElementById("mini-player");
         if (!element) {
             throw Error("no mini-player element found");
         }
 
-        const titleElement = document.getElementById("title");
+        const titleElement = document.getElementById("mini-player-title");
         if (!titleElement) {
             throw Error("no titleElement found");
         }
 
-        titleElement.textContent = "foobar";
+        titleElement.textContent = entry.title;
         element?.classList.remove("hidden");
     }
 
     close() {
-        const el = document.getElementById("mini-player")!;
-        el.classList.add("hidden");
+        const element = document.getElementById("mini-player");
+        if (!element) {
+            throw Error("no mini-player element found");
+        }
+        element.classList.add("hidden");
     }
 }
 
-export const miniPlayer = new MiniPlayer();
+export const miniPlayerInstance = new MiniPlayer();
