3e7ebdb2d5efa3cbd9256b71092ca1404925497b
garrepi <jacgarrett18@gmail.com>
2026-02-08 23:00:23 -0800

    add map layers

---
+++ src/map/layers.ts
@@ -0,0 +1,28 @@
+import { tileLayer } from "leaflet";
+
+const baseAttributes = {
+    maxZoom: 30,
+    maxNativeZoom: 17,
+};
+
+const osmLayer = tileLayer(
+    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    {
+        ...baseAttributes,
+        attribution: "© OpenStreetMap contributors",
+    },
+);
+
+const satelliteLayer = tileLayer(
+    "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+    {
+        ...baseAttributes,
+        attribution:
+            "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
+    },
+);
+
+export const mapLayers = {
+    osm: osmLayer,
+    satellite: satelliteLayer,
+};
--- src/map/map.ts
+++ src/map/map.ts
@@ -2,6 +2,7 @@ import type { POI } from "../types";
 import L from "leaflet";
 import { poiIcon } from "./components/poi-icon";
 import { poiPopup } from "./components/poi-popup";
+import { mapLayers } from "./layers";
 
 type MapConfiguartion = {
     POIs: Array<POI>;
@@ -10,26 +11,18 @@ type MapConfiguartion = {
 };
 
 export const initMap = (config: MapConfiguartion) => {
-    // TODO: make vertical
     const map = L.map("map").setView(
         config.initialLocation,
         config.initialZoom,
     );
 
-    //L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    //  attribution: "© OpenStreetMap contributors",
-    //}).addTo(map);
+    // make satellite layer the default
+    mapLayers.satellite.addTo(map);
 
-    // satallite
-    L.tileLayer(
-        "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
-        {
-            maxZoom: 30,
-            maxNativeZoom: 17,
-            attribution:
-                "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
-        },
-    ).addTo(map);
+    // TODO: orient map vertically
+
+    // drop down for layers -- TODO: only in debug mode
+    L.control.layers(mapLayers).addTo(map);
 
     config.POIs.forEach((entry, index) => {
         const { lattitude, longitude } = entry.location;
