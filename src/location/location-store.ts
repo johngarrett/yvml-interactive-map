e23e3baabfa5235fd79e9c9a964862d4ce21ddaf
garrepi <jacgarrett18@gmail.com>
2026-02-25 14:54:46 -0800

    calculate distance between points

--- src/location/location-store.ts
+++ src/location/location-store.ts
@@ -1,4 +1,5 @@
 import { debug } from "../utils";
+import L from "leaflet";
 
 type LocationPoint = {
     latitude: number;
@@ -9,7 +10,30 @@ type LocationPoint = {
 
 class LocationStore {
     maybeAdd(point: LocationPoint) {
-        // TODO: filtering around what to save based on distance
+        const previousPoint = this.data[this.data.length - 1];
+
+        if (previousPoint) {
+            let current = L.latLng(point.latitude, point.longitude);
+            let previous = L.latLng(
+                previousPoint?.latitude,
+                previousPoint?.longitude,
+            );
+
+            let distanceInMeters = current.distanceTo(previous);
+
+            debug(
+                `[LocationStore] distance between ${current} and ${previous} = ${distanceInMeters}`,
+            );
+
+            if (distanceInMeters < 1) {
+                // less than 1 meter
+                debug(
+                    `[LocationStore] distance not great enough, returning early.`,
+                );
+                return;
+            }
+        }
+
         this.data.push(point);
         debug(
             `[LocationStore]: ${point} added; total size: ${this.data.length}`,
